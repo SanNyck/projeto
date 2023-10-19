7a28648d42303fd4eea0058f31c13e4d6609700c
Nycolas Santos <nycolassantos.sc@gmail.com>
2023-10-19 02:18:06 -0300

    Tentativa

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function toggleMode() {
- const html = document.documentElement
- html.classList.toggle("light")
- 
- 
- const img = document.querySelector("#profile img")
- 
- if(html.classList.contains('light')) {
-  img.setAttribute('src','./assets/avatar.png')
- 
- }else {
-  img.setAttribute('src','./assets/avatar-light.png')
- }
- //pegar a tag img
+  const html = document.documentElement
+  html.classList.toggle("light")
 
- //se tiver light mode adicionar a imagem light
- //se tiver sem light mode , manter a imagem normal
- //substituir a imagem
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    img.setAttribute("src", "assets/avatar-light.png")
+  }
+  //pegar a tag img
+
+  //se tiver light mode adicionar a imagem light
+  //se tiver sem light mode , manter a imagem normal
+  //substituir a imagem
 }
